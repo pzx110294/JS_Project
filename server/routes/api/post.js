1e32d1a705ab22029d98c29b11423348d1685b6d
pzx110294 <193747267+pzx110294@users.noreply.github.com>
2025-05-10 20:32:34 +0200

    move '/api' in post path to higher function

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -1,7 +1,7 @@
 ﻿const express = require('express');
 const router = express.Router();
 const {createResource} = require('../../services/createResource');
-router.post('/api/resource', async (req, res) => {
+router.post('/resource', async (req, res) => {
     console.log('test ' + req.body);
     const newResource = await createResource(req.body);
     res.json(newResource);
