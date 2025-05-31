f899b54d43bc8ed8110f1383f744b5f278fce6e1
pzx110294 <193747267+pzx110294@users.noreply.github.com>
2025-05-31 19:02:57 +0200

    parse invalid authorId genreId

--- server/services/books/post.js
+++ server/services/books/post.js
@@ -3,20 +3,38 @@ const { Book, Author, Genre } = db;
 const { validateFields } = require('../../helpers/validateFields');
 async function createBook(book) {
     validateFields(book, ['Title', 'ISBN', 'AuthorId', 'GenreId'], 'Book');
-
     const newBook = await Book.create({
         Title: book.Title,
         ISBN: book.ISBN,
         PublicationDate: book.PublicationDate
     });
-    await newBook.setAuthors(book.AuthorId);
-    await newBook.setGenres(book.GenreId);
 
+    let authorIds = book.AuthorId;
+    let genreIds = book.GenreId;
+
+    if (typeof authorIds === 'string') {
+        try {
+            authorIds = JSON.parse(authorIds);
+        } catch {
+            throw new Error('Wrong authorId format');
+        }
+    }
+
+    if (typeof genreIds === 'string') {
+        try {
+            genreIds = JSON.parse(genreIds);
+        } catch {
+            throw new Error('Wrong genreId format');
+        }
+    }
+
+    await newBook.setAuthors(authorIds);
+    await newBook.setGenres(genreIds);
+    
     const result = await Book.findByPk(newBook.id, {
         include: [Author, Genre]
     });
     return result;
 }
-
 
 module.exports = { createBook };
